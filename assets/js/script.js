72e6b95268122fd2cd766e090cfd214d1ab46690
cristiandpto23 <cristian.web.development@gmail.com>
2024-07-12 01:35:27 -0400

    add cleaner code | nav problem correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,34 +2,24 @@ $(document).ready(function(){
     // Se añade a todos los elementos 'a' una función que se ejecuta con un click en ellos
     $("a").on('click', function(event) {
 
-    if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
-        var hash = this.hash;
-        // el valor numérico dice los ms que se demora en llegar al destino
-        $('html, body').animate({
-        scrollTop: $(hash).offset().top
-        }, 800, function(){
+        if (this.hash !== "") {
+            // Prevenir conducta por defecto de 'a'
+            event.preventDefault();
+            var hash = this.hash;
+            // el valor numérico dice los ms que se demora en llegar al destino
+            $('html, body').animate({
+            scrollTop: $(hash).offset().top
+            }, 800, function(){
 
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-        });
-    } // End if
+            // añade hash (#) a la URL cuando se desplaza a un destino
+            window.location.hash = hash;
+            });
+        } 
     });
 });
 
-/* $(window).on('scroll', function() {
-    // Se añade o se quita la clase 'bg-dark' al elemento 'nav' si la posición del scroll es mayor a 0
-        if(window.scrollY > 0) {
-            console.log(scrollY);
-            $('#navbar').addClass('dark-mode');
-        }
-    }
-        
-); */
-
 // Se añade los elementos que se quieren modificar (selector id)
-let nav = document.querySelector('#navbar', '#navbarTogglerDemo02');
+let nav = document.querySelector('#navbar');
 // se añade un elemento que escucha el evento scroll mayor que 100
 window.addEventListener('scroll', function() {
     if(window.scrollY > 100) {
@@ -43,8 +33,4 @@ window.addEventListener('scroll', function() {
     }
 })
 
-
-
-
-    
 
